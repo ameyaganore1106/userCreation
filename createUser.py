75f874fde3425cbebb1bda5f3f5561693322747b
Ameya Ganore <ameya.ganore@elastic.run>
2022-12-21 09:52:07 +0530

    Initial Commit

---
+++ createUser.py
@@ -0,0 +1,7 @@
+import sys
+mylist = sys.argv[1:]
+username = mylist[0]
+grpname = mylist[1]
+key = mylist[2]
+
+print("username : "+username)
---
+++ test.py
@@ -0,0 +1,57 @@
+import csv
+import subprocess
+# import paramiko
+
+
+# Reading Data from CSV 
+def readCSV():
+    with open('TestSheet .csv', mode ='r') as file:
+    
+        # reading the CSV file
+        csvFile = csv.reader(file)
+        
+        # displaying the contents of the CSV file
+        data=[]
+        for lines in csvFile:
+                user=[]
+                for line in lines:
+                    user.append(line)
+                data.append(user)
+    file.close()
+    return data
+
+
+
+
+# Copying script from remote server 
+# def copyScript():
+
+#     ssh = paramiko.SSHClient()
+#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#     ssh.connect(hostname='',username='',password='',port=22)
+#     sftp_client = ssh.open_sftp()
+
+#     sftp_client.get('source script file path','createUser.py')
+
+#     sftp_client.close()
+#     ssh.close()
+
+
+def executeScript(username,grpname,key):
+    process = subprocess.Popen(['python3','createUser.py',username,grpname,key],shell=False,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    stdout,stderr = process.communicate()
+    print(stdout)
+    
+    
+def service():
+    data=readCSV()
+    
+    dict= {}
+    for i in range(0,len(data)):
+        username = data[i][0]
+        grpname = data[i][1]
+        key = data[i][2]
+        executeScript(username,grpname,key)         
+
+service()
